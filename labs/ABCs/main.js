ca5d2f53636f2f65edc7bd8aa3df61914a001793
dimslug <dominicaltobell@gmail.com>
2023-06-11 14:32:31 -0400

    ABCs lab

---
+++ labs/ABCs/main.js
@@ -0,0 +1,29 @@
+let abcs = [
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r,','s','t','u','v','w','x','y','z'
+];
+
+console.log(abcs);
+
+let pulledLetters = [];
+
+const findLetter = () => {
+    let num = Math.floor(Math.random() * abcs.length -1);
+    console.log(num)
+
+    let returnedLtr = abcs[num];
+    abcs.splice(num,1);
+
+    pulledLetters.push(returnedLtr);
+
+    return returnedLtr.toUpperCase();
+}
+
+
+function getLetter() {
+    let ltr = findLetter()
+
+    let res = `Your letter is ${ltr}. \nHere are the pulled lettes: ${pulledLetters} \nHere is what remains: ${abcs}.`;
+
+    // return "See the return"
+    return res
+}
